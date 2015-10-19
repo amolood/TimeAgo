22b5b4d81154ee3879a92ade4aa1adfcf88aef0c
nasnasa <sudaneseh@gmail.com>
2015-10-19 22:24:30 +0300

    Create eng.php

---
+++ eng.php
@@ -0,0 +1,18 @@
+<?php
+// TimeAgo Translation file :) 
+
+$lang["second"] = "second";
+$lang["minute"] = "minute";
+$lang["hour"] = "hour";
+$lang["month"] = "month";
+$lang["year"] = "year";
+$lang["seconds"] = "seconds";
+$lang["minutes"] = "minutes";
+$lang["hours"] = "hours";
+$lang["months"] = "months";
+$lang["years"] = "years";
+$lang["ago"] = "ago";
+$lang["day"] = "day";
+$lang["days"] = "days";
+
+?>
