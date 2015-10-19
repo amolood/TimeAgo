76346df9e08593e696931265b6dfd74824a461bf
nasnasa <sudaneseh@gmail.com>
2015-10-19 23:15:48 +0300

    Create arabic.php

---
+++ arabic.php
@@ -0,0 +1,17 @@
+<?php
+// TimeAgo Translation file :) 
+$lang["second"] = "ثانية";
+$lang["minute"] = "دقيقة";
+$lang["hour"] = "ساعة";
+$lang["month"] = "شهر";
+$lang["year"] = "عام";
+$lang["seconds"] = "ثواني";
+$lang["minutes"] = "دقائق";
+$lang["hours"] = "ساعات";
+$lang["months"] = "شهور";
+$lang["years"] = "سنوات";
+$lang["ago"] = "منذ";
+$lang["day"] = "يوم";
+$lang["days"] = "ايام";
+
+?>
